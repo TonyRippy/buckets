d733144a69dab69d854926f3044ba05b9cd9fbf2
Tony Rippy <tonyrippy@gmail.com>
2026-02-12 17:11:06 -0500

    ceil bucketer

---
+++ go/buckets/ceil.go
@@ -0,0 +1,66 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buckets
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+type ceilBucketer struct {
+	Width float64
+}
+
+// CeilBucketer returns a bucketer that rounds up to the nearest multiple of width.
+func CeilBucketer(width float64) (BucketingStrategy, error) {
+	if width <= 0 {
+		return nil, fmt.Errorf("invalid width %g", width)
+	}
+	return &ceilBucketer{Width: width}, nil
+}
+
+func (b *ceilBucketer) IndexOf(value float64) (int32, error) {
+	if b.Width <= 0 {
+		return 0, fmt.Errorf("invalid width %g", b.Width)
+	}
+	index := int32(math.Ceil(value / b.Width))
+	return index, nil
+}
+
+func (b *ceilBucketer) Range(index int32) (Range, error) {
+	max := float64(index) * b.Width
+	min := max - b.Width
+	return Range{From: min, To: max, FromBound: Open, ToBound: Closed}, nil
+}
+
+func (b *ceilBucketer) String() string {
+	if b.Width == 1 {
+		return "ceil"
+	}
+	return fmt.Sprintf("ceil:width=%g", b.Width)
+}
+
+func init() {
+	RegisterParser("ceil", func(args map[string]string) (BucketingStrategy, error) {
+		width := 1.0
+		if arg, ok := args["width"]; ok {
+			var err error
+			width, err = strconv.ParseFloat(arg, 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid width %q", arg)
+			}
+		}
+		return CeilBucketer(width)
+	})
+}
